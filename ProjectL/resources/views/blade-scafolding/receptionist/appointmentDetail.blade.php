2d4d9d04f534b07035cbbb826b7428af1ad76324
anjali678 <nayananjali631@gmai.com>
2021-01-04 12:54:03 +0530

    project file

--- ProjectL/resources/views/blade-scafolding/receptionist/appointmentDetail.blade.php
+++ ProjectL/resources/views/blade-scafolding/receptionist/appointmentDetail.blade.php
@@ -15,7 +15,21 @@
     <div class="text-center" style="color: blue; font-weight: bold; font-size: 30px;">
         Appointment Details
     </div>
-	<div id="item" style="color: black; margin-left: 20%; font-weight: bold;">
+
+    <div class="serch-bar" class="text-left" style="margin-left: 17%; position: absolute; width: 250px;">
+        <div id="custom-search-input" class="table-responsive">
+            <div class="input-group col-md-12">
+                <span class="input-group-btn">
+                    <button class="btn btn-info btn-lg" type="button">
+                    <i class="fa fa-search" aria-hidden="true"></i>
+                    </button>
+                </span>
+                <input type="text" class="form-control input-lg" placeholder="Patient-ID/NIC" />
+            </div>
+        </div>
+    </div>
+
+	<div id="item" style="color: black; margin-left: 20%; font-weight: bold; margin-top: 80px;">
 
         <div>
             Date:
